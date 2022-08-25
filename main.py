464708126dfdaedb4ad7f5f0164e6036035abf38
futagoya <firdauswnn@gmail.com>
2022-08-25 14:34:24 +0700

    mencoba project

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-"""permulaan"""
+"""Semua sintaksis dasar pemrograman terdiri dari:
+1. Sekuensial: Langkah berurutan
+2. Percabangan: Langkah melompat jika kondisi terpenuhi
+3. Perulangan: Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
 
 print("Hello")
 print("who are u")
